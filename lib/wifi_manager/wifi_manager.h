4d9e7b23a6fbf21bddcf5aeb8f39dd1ac673785c
Turgut Harun Arslan <nazancakar623@gmail.com>
2025-07-14 21:22:15 +0000

    yes

---
+++ lib/wifi_manager/wifi_manager.h
@@ -0,0 +1,11 @@
+#ifndef WIFI_MANAGER_H
+#define WIFI_MANAGER_H
+
+#include <stdbool.h>
+
+#define WIFI_SSID_MAX_LEN 32
+#define WIFI_PASS_MAX_LEN 64
+
+void wifi_manager_init(void);
+
+#endif // WIFI_MANAGER_H
--- src/main.c
+++ src/main.c
@@ -1,24 +1,5 @@
 #include <stdio.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-
-void print_task(void *pvParameter)
-{
-    int count = 0;
-    while (1) {
-        printf("Task running, count = %d\n", count++);
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay 1 second
-    }
-}
 
 void app_main(void)
 {
-    xTaskCreate(
-        print_task,     // Task function
-        "Print Task",   // Name of the task (for debugging)
-        2048,           // Stack size in words
-        NULL,           // Task input parameter
-        5,              // Priority
-        NULL            // Task handle
-    );
 }
